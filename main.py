3833f1827a9405a2e8073ff46f2c2b7476204c19
SusXhail1 <huangxiaolei864@gmail.com>
2024-10-22 18:56:13 -0400

    adjusted code and finished

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 #author - Xiaolei Huang
 
+def main():
+    encoded_password = None
+    go = True
+    while go:  # main loop
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print()
+        option = input('Please enter an option: ')
+        if option == '1':  # encoder
+            pw = input("Please enter your password to encode: ")
+            encoded_password = encode(pw)
+            print('Your password has been encoded and stored!')
+            print()
+        if option == '2':  # decoder
+            if encoded_password is None:  # If no password encoded, tell user to enter a password to option 1
+                print("No encoded password found. Please encode a password first.")
+                print()
+            else:  # Decodes password and prints out encoded password and original password
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+                print()
+        if option == "3":  # quit function
+            quit()
+
 def encode(password):
     encoded_password= ""
     for digit in password: #converts each digit on a char by char basis
@@ -35,33 +62,4 @@ def decode(encoded_password):
 
 
 if __name__== '__main__':
-    """Comment by Nicholas: put whats under here in a main function like 
-    def main():
-        code
-    Then whats ran is main() after if __name__== '__main__':
-    """
-    encoded_password = None
-    go= True
-    while go: #main loop
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')
-        print()
-        option= input('Please enter an option: ')
-        if option== '1': #encoder
-            pw= input("Please enter your password to encode: ")
-            encoded_password= encode(pw)
-            print('Your password has been encoded and stored!')
-            print()
-        if option=='2': #decoder
-            if encoded_password is None:    # If no password encoded, tell user to enter a password to option 1
-                print("No encoded password found. Please encode a password first.")
-                print()
-            else:   # Decodes password and prints out encoded password and original password
-                decoded_password = decode(encoded_password)
-                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
-                print()
-        if option=="3": #quit function
-            quit()
+    main()
